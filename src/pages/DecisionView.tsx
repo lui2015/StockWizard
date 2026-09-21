ff58dd5843d974fa4e115050f34dc48310fdd91c
鲁力铭 <lui2015@users.noreply.github.com>
2026-09-21 20:53:34 +0800

    fix: 训练家决策 iframe src 加尾斜杠，修复内嵌 404

--- src/pages/DecisionView.tsx
+++ src/pages/DecisionView.tsx
@@ -1,5 +1,5 @@
 import { EmbedPage } from '../components/EmbedPage'
 
 export function DecisionView() {
-  return <EmbedPage title="训练家决策" subtitle="训练家的投资决策助手" src="/investmentDecision" />
+  return <EmbedPage title="训练家决策" subtitle="训练家的投资决策助手" src="/investmentDecision/" />
 }
